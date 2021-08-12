4ede1751da01099fa64e7b636e11f4a8d9030c8c
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2021-08-12 21:03:54 +0200

    Added identity operator

--- src/concrete_type.jl
+++ src/concrete_type.jl
@@ -1,7 +1,10 @@
 #: Concrete type
 
 export LinearOperator, linear_operator
+export IdentityOperator, identity_operator
 
+
+# Generic linear operator
 
 struct LinearOperator{DT,RT}<:AbstractLinearOperator{DT,RT}
     domain_size::NTuple{<:Any,Int64}
@@ -16,3 +19,17 @@ matvecprod(L::LinearOperator{DT,RT}, u::DT) where {DT,RT} = L.matvecprod(u)
 matvecprod_adj(L::LinearOperator{DT,RT}, v::RT) where {DT,RT} = L.matvecprod_adj(v)
 
 linear_operator(DT::DataType, RT::DataType, domain_size::NTuple{N1,Int64}, range_size::NTuple{N2,Int64}, matvecprod::Function, matvecprod_adj::Function) where {N1,N2} = LinearOperator{DT,RT}(domain_size, range_size, matvecprod, matvecprod_adj)
+
+
+# Identity
+
+struct IdentityOperator{DT}<:AbstractLinearOperator{DT,DT}
+    size::NTuple{<:Any,Int64}
+end
+
+domain_size(I::IdentityOperator{DT}) where DT = I.size
+range_size(I::IdentityOperator{DT}) where DT = I.size
+matvecprod(::IdentityOperator{DT}, u::DT) where DT = u
+matvecprod_adj(::IdentityOperator{DT}, v::DT) where DT = v
+
+identity_operator(DT::DataType, size::NTuple{N,Int64}) where N = IdentityOperator{DT}(size)
