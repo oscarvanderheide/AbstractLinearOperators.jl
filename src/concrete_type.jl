65a159f6ac96f663ef33eb7d1d390805412f6dda
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2022-04-08 15:00:53 +0200

    Correct mispelling

--- src/concrete_type.jl
+++ src/concrete_type.jl
@@ -16,7 +16,7 @@ domain_size(L::LinearOperator) = L.domain_size
 range_size(L::LinearOperator) = L.range_size
 matvecprod(L::LinearOperator{T,ND,NR}, u::AbstractArray{T,ND}) where {T,ND,NR} = L.matvecprod(u)
 matvecprod_adj(L::LinearOperator{T,ND,NR}, v::AbstractArray{T,NR}) where {T,ND,NR} = L.matvecprod_adj(v)
-invmatvecprod(L::LinearOperator{T,ND,NR}, u::AbstractArray{T,NR}) where {T,ND,NR} = ~inothing(L.invmatvecprod) && L.invmatvecprod(u)
-invmatvecprod_adj(L::LinearOperator{T,ND,NR}, v::AbstractArray{T,ND}) where {T,ND,NR} = ~inothing(L.invmatvecprod_adj) && L.invmatvecprod_adj(v)
+invmatvecprod(L::LinearOperator{T,ND,NR}, u::AbstractArray{T,NR}) where {T,ND,NR} = ~isnothing(L.invmatvecprod) && L.invmatvecprod(u)
+invmatvecprod_adj(L::LinearOperator{T,ND,NR}, v::AbstractArray{T,ND}) where {T,ND,NR} = ~isnothing(L.invmatvecprod_adj) && L.invmatvecprod_adj(v)
 
 linear_operator(T::DataType, domain_size::NTuple{ND,Int64}, range_size::NTuple{NR,Int64}, matvecprod::Function, matvecprod_adj::Function; invmatvecprod::Union{Nothing,Function}=nothing, invmatvecprod_adj::Union{Nothing,Function}=nothing) where {ND,NR} = LinearOperator{T,ND,NR}(domain_size, range_size, matvecprod, matvecprod_adj, invmatvecprod, invmatvecprod_adj)
