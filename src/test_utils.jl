2fe24c63dccc2a8630b59b5da122103ce8d099a7
= <=>
2023-09-01 14:01:55 +0200

    Inverse test fix

--- src/test_utils.jl
+++ src/test_utils.jl
@@ -1,6 +1,6 @@
 #: Utilities for testing operators
 
-export adjoint_test, inverse_test
+export adjoint_test, inverse_test, left_inverse_test, right_inverse_test
 
 const RealOrComplex{T<:Real} = Union{T, Complex{T}}
 
@@ -19,22 +19,38 @@ end
 function inverse_test(A::AbstractLinearOperator{CTD,ND,CTR,NR};
                       input::Union{Nothing,AbstractArray{CTD,ND}}=nothing,
                       output::Union{Nothing,AbstractArray{CTR,NR}}=nothing,
-                      rtol::Union{Nothing,TD,TR}=nothing,
-                      type::String="both") where {TD<:AbstractFloat,ND,TR<:AbstractFloat,NR,CTD<:RealOrComplex{TD},CTR<:RealOrComplex{TR}}
+                      rtol::Union{Nothing,TD,TR}=nothing) where
+                      {TD<:AbstractFloat,ND,TR<:AbstractFloat,NR,CTD<:RealOrComplex{TD},CTR<:RealOrComplex{TR}}
 
     isnothing(input)  && (input  = randn(CTD, domain_size(A)))
     isnothing(output) && (output = randn(CTR, range_size(A)))
     isnothing(rtol) && (rtol = findmax((eps(TD), eps(TR)))[1])
     Ainv = inv(A)
-    if (type == "both")
-        return isapprox(Ainv*(A*input), input; rtol=rtol) &&
-               isapprox(A*(Ainv*output), output; rtol=rtol)
-    elseif (type == "left")
-        return isapprox(Ainv*(A*input), input; rtol=rtol)
-    elseif (type == "right")
-        return isapprox(A*(Ainv*output), output; rtol=rtol)
-    else
-        return error("Type must be left, right, or both")
-    end
+    return isapprox(Ainv*(A*input), input; rtol=rtol) &&
+           isapprox(A*(Ainv*output), output; rtol=rtol)
+
+end
+
+function left_inverse_test(A::AbstractLinearOperator{CTD,ND,CTR,NR};
+                           input::Union{Nothing,AbstractArray{CTD,ND}}=nothing,
+                           rtol::Union{Nothing,TD,TR}=nothing) where
+                           {TD<:AbstractFloat,ND,TR<:AbstractFloat,NR,CTD<:RealOrComplex{TD},CTR<:RealOrComplex{TR}}
+
+    isnothing(input)  && (input  = randn(CTD, domain_size(A)))
+    isnothing(rtol) && (rtol = findmax((eps(TD), eps(TR)))[1])
+    Ainv = inv(A)
+    return isapprox(Ainv*(A*input), input; rtol=rtol)
+
+end
+
+function right_inverse_test(A::AbstractLinearOperator{CTD,ND,CTR,NR};
+                            output::Union{Nothing,AbstractArray{CTR,NR}}=nothing,
+                            rtol::Union{Nothing,TD,TR}=nothing) where
+                            {TD<:AbstractFloat,ND,TR<:AbstractFloat,NR,CTD<:RealOrComplex{TD},CTR<:RealOrComplex{TR}}
+
+    isnothing(output) && (output  = randn(CTR, range_size(A)))
+    isnothing(rtol) && (rtol = findmax((eps(TD), eps(TR)))[1])
+    Ainv = inv(A)
+    return isapprox(A*(Ainv*output), output; rtol=rtol)
 
 end
